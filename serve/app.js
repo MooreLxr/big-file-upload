6f575e4f485be4f1483c0063fa78ba4ff32b65c4
MooreLxr <1052777399@qq.com>
2022-03-22 23:10:58 +0800

    修改 切片上传接口

--- serve/app.js
+++ serve/app.js
@@ -19,8 +19,8 @@ app.use(logger('dev'))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 //服务器提交的数据json化
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
 
 app.use('/', indexRouter)
 
--- serve/routes/index.js
+++ serve/routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const path = require('path')
+const fs = require('fs')
 const multiparty = require('multiparty')
 
 /* GET home page. */
@@ -8,55 +9,67 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' })
 })
 
-router.post('/getName', (req, res, next) => {
-  console.log('dadadad', req.body.name)
-  res.json({
-    data: req.name,
-    message: '成功',
-    code: 0
-  })
-})
 /**
  * 上传切片
  * file:文件流  fileId:文件id  fileIndex:切片索引
  */
 router.post('/uploadSlice', (req, res, next) => {
-  console.log('----', req.body)
   // const { file, fileId, fileIndex } = req.body
   // const chunkUploadDir = path.join(uploadPath, fileId, '/') // 切片上传目录
   // const chunkFileName = fileId + '-' + fileIndex // 切片名称
-  // const form = new multiparty.Form()
-  // form.encoding = 'utf-8'
-  // form.uploadDir = '../public/upload_files/' //设置文件存储路径
-  // form.maxFilesSize = 10 * 1024 * 1024 // 单文件大小限制：10M
-  // form.parse(req, (err, fields, files) => {
-  //   console.log('fields', fields)
-  //   console.log('files', files)
-  //   if (err) {
-  //     console.log(err)
-  //     res.json({
-  //       data: '',
-  //       message: '上传失败',
-  //       code: 0
-  //     })
-  //     return false
-  //   } else {
-
-  //   }
-  // })
-  // uploadFileFn(file, chunkUploadDir, chunkFileName).then(response => {
-  //   res.json({
-  //     data: '',
-  //     message: '上传完成',
-  //     code: 1
-  //   })
-  // }).catch(err => {
-  //   res.json({
-  //     data: '',
-  //     message: '上传失败',
-  //     code: 0
-  //   })
-  // })
+  const form = new multiparty.Form()
+  form.encoding = 'utf-8'
+  form.uploadDir = './public/upload_files/' //设置文件存储路径
+  form.maxFilesSize = 10 * 1024 * 1024 // 单文件大小限制：10M
+  form.parse(req, (err, fields, files) => {
+    // fields {
+    //   fileId: [ 'aacac792-6f28-4010-ad62-01d0bbe8d464' ],
+    //   fileIndex: [ '0' ]
+    // }
+    // files {
+    //   file: [
+    //     {
+    //       fieldName: 'file',
+    //       originalFilename: 'blob',
+    //       path: 'public\\upload_files\\uillblIiB7v3o_j8qLDhkNbg',
+    //       headers: [Object],
+    //       size: 2819779
+    //     }
+    //   ]
+    // }
+    console.log('fields', fields)
+    console.log('files', files)
+    if (err) {
+      res.json({
+        data: '',
+        message: '上传失败',
+        code: 0
+      })
+      return false
+    } else {
+      const { fileId, fileIndex } = fields
+      const inputFile = files.file[0]
+      const oldName = inputFile.path
+      const chunkName =  './public/upload_files/' + fileId[0] + '-' + fileIndex[0]// 切片名称
+      //重命名为真实文件名
+      fs.rename(oldName, chunkName, function (err) {
+        if (err) {
+          console.log('rename error: ' + err)
+          res.json({
+            data: '',
+            message: '上传失败',
+            code: 0
+          })
+        } else {
+          res.json({
+            data: chunkName,
+            message: '上传成功',
+            code: 1
+          })
+        }
+      })
+    }
+  })
 })
 
 /**
